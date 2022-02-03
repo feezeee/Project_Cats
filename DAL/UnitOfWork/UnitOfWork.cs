3365cd52090e4fde9f9f29d28d7d34e8bc69c64e
Денис Скурат <den.skur2016@yandex.ru>
2022-02-03 16:49:23 +0300

    UnitOfWork

---
+++ DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using BLL.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        CatContext db;
+        public UnitOfWork(CatContext context)
+        {
+            db = context;
+        }
+
+        public async Task SaveAsync()
+        {
+            await db.SaveChangesAsync();
+        }
+    }
+}

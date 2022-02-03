457b24676bb3a5e7fc6e951bed4635b7b900d212
Денис Скурат <den.skur2016@yandex.ru>
2022-02-03 19:01:47 +0300

    Добавил GetCatResponse

---
+++ Cat.API/Response/GetCatResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Cat.API.Response
+{
+    public class GetCatResponse
+    {
+        public string Name { get; set; }      
+        public DateTime DateOfBirth { get; set; }        
+        public double Price { get; set; }
+    }
+}

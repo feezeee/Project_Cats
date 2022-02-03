69a609ff0b2c594f33c89edf648a9e9f4a663e18
Денис Скурат <den.skur2016@yandex.ru>
2022-02-03 19:01:09 +0300

    Добавил папку Request и в ней нужные классы для методов

---
+++ Cat.API/Request/DeleteCatRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Cat.API.Request
+{
+    public class DeleteCatRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Cat.API/Request/PostCatRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Cat.API.Request
+{
+    public class PostCatRequest
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public double Price { get; set; }
+    }
+}

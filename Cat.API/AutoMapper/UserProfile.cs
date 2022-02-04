835c939f057a11a57021f5bb43cb0047c0af4d59
Денис Скурат <den.skur2016@yandex.ru>
2022-02-04 20:49:46 +0300

    Добавил новые профили в автомапер

---
+++ Cat.API/AutoMapper/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Cat.API.Request;
+using Cat.API.Response;
+
+namespace Cat.API.AutoMapper
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<BLL.Entities.Account, UserIdentityResponse>();
+            CreateMap<PostUserRequest, BLL.Entities.Account>();
+            CreateMap<DeleteUserRequest, BLL.Entities.Account>();
+            CreateMap<PutUserRequest, BLL.Entities.Account>();
+        }
+    }
+}

f80b98a0275058748f2a655769538cf57c5bb603
Денис Скурат <den.skur2016@yandex.ru>
2022-02-04 20:51:21 +0300

    Добавил JWTMiddleware и Authorize атрибут

---
+++ Cat.API/Middleware/JWTMiddleware.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using BLL.Services;
+using Cat.API.Response;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Cat.API.Middleware
+{
+    public class JWTMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+        private readonly IAccountService _userService;
+        private readonly IMapper _mapper;
+        public JWTMiddleware(RequestDelegate next, IConfiguration configuration, IAccountService userService, IMapper mapper)
+        {
+            _next = next;
+            _configuration = configuration;
+            _userService = userService;
+            _mapper = mapper;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+                attachAccountToContext(context, token);
+
+            await _next(context);
+        }
+
+        private void attachAccountToContext(HttpContext context, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = _configuration["Jwt:Issuer"],
+                    //ValidateAudience = false,
+                    ValidateAudience = true,
+                    ValidAudience = _configuration["Jwt:Audience"],
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var accoutnname = jwtToken.Claims.First(x => x.Type == ClaimsIdentity.DefaultNameClaimType).Value;
+
+                
+                // attach account to context on successful jwt validation
+                context.Items["User"] = _mapper.Map<UserIdentityResponse>(_userService.GetByName(accoutnname).Result);
+            }
+            catch
+            {
+                // do nothing if jwt validation fails
+                // account is not attached to context so request won't have access to secure routes
+            }
+        }
+    }
+}

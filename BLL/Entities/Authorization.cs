0b95e9cfad8e2d5874b4ac3a9f6ae0da67b0cff0
Денис Скурат <den.skur2016@yandex.ru>
2022-02-08 00:31:51 +0300

    Изменения в BLL Entities

--- BLL/Entities/Authorization.cs
+++ BLL/Entities/Authorization.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BLL.Entities
 {
-    public class Account
+    public class Authorization
     {
-        public string Login { get; set; }        
-        public string Password { get; set; }        
+        public string Login { get; set; }
         public string Role { get; set; }
+        public string Token { get; set; }
+
     }
 }

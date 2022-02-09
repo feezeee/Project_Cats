82837ef4f6934334eaf3b5a2c83a8903dc7fd960
Denis <d.s.skurat@gmail.com>
2022-02-09 19:44:41 +0300

    Обновил Entity

--- BLL/Entities/RefreshToken.cs
+++ BLL/Entities/RefreshToken.cs
@@ -17,11 +17,12 @@ namespace BLL.Entities
         public string CreatedByIp { get; set; }
         public DateTime Created { get; set; }
         public DateTime Expires {get; set; }
+        public DateTime? Revoked { get; set; }
+        public string? RevokedByIp { get; set; }
+        public string? ReplacedByToken { get; set; }
         public bool IsExpired => DateTime.UtcNow >= Expires;
         public bool IsActive => Revoked == null && !IsExpired;
-        public DateTime? Revoked    {get; set; }
-        public string RevokedByIp { get; set; }
-        public string ReplacedByToken { get; set; }
+        
         
     }
 }
